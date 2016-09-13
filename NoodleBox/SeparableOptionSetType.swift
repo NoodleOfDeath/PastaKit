10257592cae61bd7751f1edb0a36e83dc71b5fd4
Thom Morgan <github@bytemeapp.com>
2016-09-13 12:42:38 -0400

    SeparableOptionSetType

---
+++ NoodleBox/SeparableOptionSetType.swift
@@ -0,0 +1,61 @@
+//
+//  StringOptionSetType.swift
+//  NoodleBox
+//
+//  Created by Thom Morgan on 9/12/16.
+//  Copyright © 2016 NoodleNation. All rights reserved.
+//
+
+import Foundation
+
+public protocol Separable : Hashable {
+    func separatedComponents() -> [Self]
+}
+
+extension String : Separable {}
+
+extension String {
+    public func separatedComponents() -> [String] {
+        return componentsSeparatedByCharactersInSet(.whitespaceCharacterSet())
+    }
+}
+
+public protocol SeparableOptionSetType : OptionSetType {
+    
+    associatedtype Key: Separable
+    static var values: [Key : Self] { get }
+    
+    init(rawValue: RawValue)
+    init(_ rawValue: RawValue)
+    
+    init(key: Key?)
+    init(_ key: Key?)
+    init(keys: [Key]?)
+    init(_ keys: [Key]?)
+    
+}
+
+extension SeparableOptionSetType {
+    
+    public init(_ rawValue: RawValue) {
+        self.init(rawValue: rawValue)
+    }
+    
+    public init(key: Key?) {
+        self.init(keys: key?.separatedComponents())
+    }
+    
+    public init(_ key: Key?) {
+        self.init(key: key)
+    }
+    
+    public init(keys: [Key]?) {
+        self.init()
+        guard let keys = keys else { return }
+        for key in keys {
+            guard let value = Self.values[key] else { continue }
+            insert(value)
+        }
+    }
+    
+}
