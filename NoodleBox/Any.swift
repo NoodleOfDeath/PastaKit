1f89d2ef143be86ea3925410b3b70b37896bf223
Thom Morgan <thomasmorgan@go.rmc.edu>
2017-03-25 15:23:20 -0400

    Reopened Project

--- NoodleBox/Any.swift
+++ NoodleBox/Any.swift
@@ -24,7 +24,8 @@
 /// - parameter lhs: assignment target.
 /// - parameter rhs: value to assign to `lhs`.
 public func =? <T>(lhs: inout T?, rhs: T?) {
-    lhs! =? rhs
+    guard let rhs = rhs else { return }
+    lhs = rhs
 }
 
 /// Optionally assigns `rhs` to `lhs`, iff `rhs != nil`.
